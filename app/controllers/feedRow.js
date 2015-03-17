7b3f25ed9b4ac6e0d424088fa8f21c6e791bd9dd
WestTexasAM <leeland_h@hotmail.com>
2015-03-16 23:49:17 -0500

    Initial

---
+++ app/controllers/feedRow.js
@@ -0,0 +1,14 @@
+var args = arguments[0] || {};
+$.image.image = model.attributes.urls.preview;
+$.titleLabel.text = args.title || "";
+$.row_id = model.id || "";
+
+
+
+
+//var args = arguments[0] || {};
+
+// this is setting the view elements of the row view
+// based on the arguments passed into the contrller
+//$.image.image = args.image;
+//$.titleLabel.text = args.title || ";
