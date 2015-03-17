fa700a52132727f54338375b5f08c959b0ec6537
WestTexasAM <leeland_h@hotmail.com>
2015-03-17 00:51:50 -0500

    Creating the Photo Model
    Modifying the ACS Sync Adapter to Support the Photo Model 1947

---
+++ app/models/user.js
@@ -0,0 +1,26 @@
+exports.definition = {
+	config: {
+
+		"adapter": {
+			"type": "acs",
+			"collection_name": "users"
+		}
+	},
+	
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			// extended functions go here
+		});
+		// end extend
+		return Model;
+	},
+	extendCollection : function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions go here
+			
+		});
+		// end extend
+		
+		return Collection;
+	},
+};
