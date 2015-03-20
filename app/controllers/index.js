8b6f0b16ee3debc5e8ff61429237ad9db460b4fb
WestTexasAM <leeland_h@hotmail.com>
2015-03-20 14:40:30 -0500

    User Login with User Model
    1976

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -27,3 +27,17 @@ function doOpen() {
 }
 
 $.tabGroup.open();
+
+// when we start up, create a user and log in
+var user = Alloy.createModel('User');
+
+// we are using the default administration account for now
+user.login("wileytigram_admin", "wileytigram_admin", function(_response) {
+	if (_response.success) {
+		// open the main screen
+		$.index.open();
+	} else {
+		alert("Error Starting Application " + _response.error);
+		Ti.API.error('error logging in ' + _response.error);
+	}
+});
